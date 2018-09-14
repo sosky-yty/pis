836e52cbb908fd29c67d46bdbad704cc6e1d69fd
sosky-yty <1027797811@qq.com>
2018-09-14 21:44:23 +0800

    合并

--- app/src/main/java/com/example/sosky/pis_copy/ui/addKeyFamilyActivity.java
+++ app/src/main/java/com/example/sosky/pis_copy/ui/addKeyFamilyActivity.java
@@ -1,10 +1,11 @@
 package com.example.sosky.pis_copy.ui;
 
+import com.example.sosky.pis_copy.R;
 import com.example.sosky.pis_copy.base.BaseActivity;
 
 public class addKeyFamilyActivity extends BaseActivity{
     @Override
     protected int getContentID() {
-        return 0;
+        return R.layout.activity_addkeyfamily;
     }
 }
