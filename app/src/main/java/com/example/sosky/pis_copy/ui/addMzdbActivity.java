3b81e3e0ff4f18b72bcae47b65a43a82a8fe333b
sosky-yty <1027797811@qq.com>
2018-09-15 20:42:10 +0800

    临时　低保完成

--- app/src/main/java/com/example/sosky/pis_copy/ui/addMzdbActivity.java
+++ app/src/main/java/com/example/sosky/pis_copy/ui/addMzdbActivity.java
@@ -6,29 +6,39 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Switch;
 
+import com.example.sosky.pis_copy.MyTools;
 import com.example.sosky.pis_copy.R;
 import com.example.sosky.pis_copy.SaveTool;
+import com.example.sosky.pis_copy.adapter.LocalFamilyAdapter;
 import com.example.sosky.pis_copy.base.BaseActivity;
+import com.example.sosky.pis_copy.bean.UpLowInsuranceBean;
 import com.example.sosky.pis_copy.bean.UpPovertyBean;
 import com.example.sosky.pis_copy.bean.UpVeryStrickenBean;
 import com.google.gson.Gson;
 import com.vondear.rxtools.RxLogTool;
 import com.vondear.rxtools.view.RxTitle;
+import com.vondear.rxtools.view.RxToast;
 
 import java.util.Map;
 
+/**
+ * 低保
+ */
 public class addMzdbActivity extends BaseActivity {
+    private String [] dblb={"低保兜底","贫困低保","一般低保"};
+
+
     private RxTitle rxTitle;
     private EditText mzdbOrdHz;
     private EditText mzdbOrdHzsfz;
-    private Switch mzdbOrdXf;
+    private Switch mzdbOrdSfxsdb;
     private EditText mzdbOrdXsnf;
     private EditText mzdbOrdDblb;
     private EditText mzdbOrdXsje;
     private EditText mzdbOrdBz;
     private Button btnSaveClxx;
     private String mID;
-    private UpVeryStrickenBean.InfoBean mInfoBean = new UpVeryStrickenBean.InfoBean();
+    private UpLowInsuranceBean.InfoBean mInfoBean = new UpLowInsuranceBean.InfoBean();
 
 
     @Override
@@ -44,10 +54,10 @@ public class addMzdbActivity extends BaseActivity {
     protected void loadDatas() {
         RxLogTool.e("开始加载本地");
         try {
-            Map<String, String> veryStrickenMap = SaveTool.getVeryStricken();
-            String json = veryStrickenMap.get(mID);
-            UpVeryStrickenBean upVeryStrickenBean = new Gson().fromJson(json, UpVeryStrickenBean.class);
-            mInfoBean = upVeryStrickenBean.getInfoBeans().get(0);
+            Map<String, String> lowInsuranceMap = SaveTool.getLowInsurance();
+            String json = lowInsuranceMap.get(mID);
+            UpLowInsuranceBean lowInsuranceBean = new Gson().fromJson(json, UpLowInsuranceBean.class);
+            mInfoBean = lowInsuranceBean.getInfoBeans().get(0);
             if (mInfoBean != null) {
                 inputDatas();
             }
@@ -62,23 +72,31 @@ public class addMzdbActivity extends BaseActivity {
         rxTitle.setLeftFinish(this);
         mzdbOrdHz = (EditText) findViewById(R.id.mzdb_ord_hz);
         mzdbOrdHzsfz = (EditText) findViewById(R.id.mzdb_ord_hzsfz);
-        mzdbOrdXf = (Switch) findViewById(R.id.mzdb_ord_xf);
+        mzdbOrdSfxsdb = (Switch) findViewById(R.id.mzdb_ord_sfxsdb);
         mzdbOrdXsnf = (EditText) findViewById(R.id.mzdb_ord_xsnf);
         mzdbOrdDblb = (EditText) findViewById(R.id.mzdb_ord_dblb);
         mzdbOrdXsje = (EditText) findViewById(R.id.mzdb_ord_xsje);
         mzdbOrdBz = (EditText) findViewById(R.id.mzdb_ord_bz);
         btnSaveClxx = (Button) findViewById(R.id.btn_save_clxx);
 
+
     }
 
     @Override
     protected void bindListener() {
+        mzdbOrdDblb.setOnClickListener(view -> {
+            MyTools.showSelectDialog(dblb,mContext,mzdbOrdDblb);
+        });
 
-
-
-
-
-
+        btnSaveClxx.setOnClickListener(view -> {
+            UpLowInsuranceBean.InfoBean bean = saveDatas();
+            if (MyTools.verificationID(bean.getOrd2_hzsfz())) {
+                SaveTool.saveOneLowInsurance(bean);
+                RxToast.success("保存成功");
+            } else {
+                RxToast.error("身份证错误,无法保存");
+            }
+        });
 
     }
 
@@ -86,15 +104,29 @@ public class addMzdbActivity extends BaseActivity {
     protected void inputDatas() {
          mzdbOrdHz   .setText(mInfoBean.getOrd2_bz());
          mzdbOrdHzsfz.setText(mInfoBean.getOrd2_bz());
-         mzdbOrdXf   .setText(mInfoBean.getOrd2_bz());
          mzdbOrdXsnf .setText(mInfoBean.getOrd2_bz());
          mzdbOrdDblb .setText(mInfoBean.getOrd2_bz());
          mzdbOrdXsje .setText(mInfoBean.getOrd2_bz());
          mzdbOrdBz   .setText(mInfoBean.getOrd2_bz());
+         mzdbOrdSfxsdb .setChecked(mInfoBean.getOrd2_sfxsdb().equals("是"));
     }
+
 
     @Override
     protected int getContentID() {
         return R.layout.activity_form_mzdb;
+    }
+
+
+    private UpLowInsuranceBean.InfoBean saveDatas(){
+        UpLowInsuranceBean.InfoBean infoBean = new UpLowInsuranceBean.InfoBean();
+         infoBean.setOrd2_hz   (mzdbOrdHz       .getText().toString());
+         infoBean.setOrd2_hzsfz   (mzdbOrdHzsfz    .getText().toString());
+         infoBean.setOrd2_xsnf   (mzdbOrdXsnf     .getText().toString());
+         infoBean.setOrd2_dblb   (mzdbOrdDblb     .getText().toString());
+         infoBean.setOrd2_xsje   (mzdbOrdXsje     .getText().toString());
+         infoBean.setOrd2_bz   (mzdbOrdBz       .getText().toString());
+         infoBean.setOrd2_sfxsdb   (mzdbOrdSfxsdb.isChecked()?"是":"否");
+        return infoBean;
     }
 }
