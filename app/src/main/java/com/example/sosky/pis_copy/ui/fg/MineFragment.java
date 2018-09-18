329a7e135620d07e581dc9f0b47c9bcb1e08262a
sosky-yty <1027797811@qq.com>
2018-09-18 13:15:14 +0800

    fix  dialog

--- app/src/main/java/com/example/sosky/pis_copy/ui/fg/MineFragment.java
+++ app/src/main/java/com/example/sosky/pis_copy/ui/fg/MineFragment.java
@@ -123,6 +123,7 @@ public class MineFragment extends BaseFragment {
 
                         }
                     });
+                    dialogSure.dismiss();
                 }
             });
             dialogSure.show();
